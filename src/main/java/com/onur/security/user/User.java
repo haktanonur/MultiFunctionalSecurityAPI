3d6b1680a6b581ec5b10bf6abe02cb71fb910435
Onur Haktan <onurhaktan243@gmail.com>
2023-10-04 14:29:23 +0300

    add user

---
+++ src/main/java/com/onur/security/user/User.java
@@ -0,0 +1,31 @@
+package com.onur.security.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_user")
+public class User {
+
+    @Id
+    @GeneratedValue
+    private String id;
+
+    private String firstname;
+
+    private String lastname;
+
+    private String email;
+
+    private String password;
+}
