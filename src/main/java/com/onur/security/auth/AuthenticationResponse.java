7a4d159c15d3cae268b4b0358b9d0a8e91c4026e
Onur Haktan <onurhaktan243@gmail.com>
2023-11-23 14:21:58 +0300

    extend Authentication Response

--- src/main/java/com/onur/security/auth/AuthenticationResponse.java
+++ src/main/java/com/onur/security/auth/AuthenticationResponse.java
@@ -1,5 +1,6 @@
 package com.onur.security.auth;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,5 +11,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class AuthenticationResponse {
-    private String token;
+
+    @JsonProperty("access_token")
+    private String accessToken;
+    @JsonProperty("refresh_token")
+    private String refreshToken;
 }
