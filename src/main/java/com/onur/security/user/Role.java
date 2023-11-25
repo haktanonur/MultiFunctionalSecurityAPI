f12660759a0437bc2449a2585cd36b538b93c4be
Onur Haktan <onurhaktan243@gmail.com>
2023-11-25 23:26:04 +0300

    create roles and permissions

--- src/main/java/com/onur/security/user/Role.java
+++ src/main/java/com/onur/security/user/Role.java
@@ -1,6 +1,52 @@
 package com.onur.security.user;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+import static com.onur.security.user.Permission.*;
+
+
+@RequiredArgsConstructor
 public enum Role {
-    USER,
-    ADMIN
+    USER(Collections.emptySet()),
+    ADMIN(
+            Set.of(
+                    ADMIN_READ,
+                    ADMIN_CREATE,
+                    ADMIN_UPDATE,
+                    ADMIN_DELETE,
+                    MANAGER_READ,
+                    MANAGER_CREATE,
+                    MANAGER_UPDATE,
+                    MANAGER_DELETE
+            )
+    ),
+    MANAGER(
+            Set.of(
+                    MANAGER_READ,
+                    MANAGER_CREATE,
+                    MANAGER_UPDATE,
+                    MANAGER_DELETE
+            )
+    )
+    ;
+
+    // I don't want to have duplications on the permissions
+    // so for example if I have admin:read or admin:update two times it will accept only one
+    @Getter
+    private final Set<Permission> permissions;
+
+    public List<SimpleGrantedAuthority> getAuthorities(){
+        var authorities = getPermissions()
+                .stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.name()))
+                .toList();
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+        return authorities;
+    }
 }
