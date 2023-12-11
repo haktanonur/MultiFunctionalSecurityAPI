8ad204a9b09b603ecb152323aee3b3531ce0c8a7
Onur Haktan <onurhaktan243@gmail.com>
2023-12-11 18:49:01 +0300

    setup book endpoint

---
+++ src/main/java/com/onur/security/book/BookService.java
@@ -0,0 +1,19 @@
+package com.onur.security.book;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BookService {
+
+    private final BookRepository repository;
+
+    public void save(BookRequest request) {
+        var book = Book.builder()
+                .author(request.getAuthor())
+                .isbn(request.getIsbn())
+                .build();
+        repository.save(book);
+    }
+}
